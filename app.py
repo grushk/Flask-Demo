7a089af6b95d59388e080e52131f00dc0c2cb102
Grush Khalsa <grushk@gmail.com>
2016-12-27 11:30:55 -0800

    draft

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 import requests, pandas, simplejson
 from bokeh.plotting import figure
 from bokeh.embed import components
+from bokeh.resources import INLINE
+from bokeh.templates import RESOURCES
 
 
 app = Flask(__name__)
@@ -34,12 +36,19 @@ def plotchart():
 	df = df.set_index(['Date'])
 	df.index = pandas.to_datetime(df.index)
 
+	plot_resources = RESOURCES.render(
+		s_raw=INLINE.js_raw,
+		css_raw=INLINE.css_raw,
+		js_files=INLINE.js_files,
+		css_files=INLINE.css_files
+	)
+
 	plot = figure(tools=TOOLS,
 				title='Data from Quandle WIKI set',
 				x_axis_label='date',
 				x_axis_type='datetime')
 	script, div = components(plot)
-	return render_template('index.html', script=script, div=div)
+	return render_template('index.html', script=script, div=div, plot_resources=plot_resources)
 
 
 if __name__ == '__main__':
